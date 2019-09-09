0dbbecd82c7db082ff0341fbd141b1f2ed8d1c7e
minhan <alsgks1218@gmail.com>
2019-09-10 07:30:16 +0900

    first component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Greeting({ food, beverage }) {
+  console.log();
+  return (
+    <h3>
+      this is {food} and {beverage}
+    </h3>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HELLO WORLD</h1>
+      <Greeting food="bread" beverage="water" />
+      <Greeting food="kimchi" beverage="saida" />
+      <Greeting food="meet" beverage="cola" />
     </div>
   );
 }
