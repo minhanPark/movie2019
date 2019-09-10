80431077e74561e68865b6caced29a42ab304e24
unknown <alsgks1218@naver.com>
2019-09-10 11:28:35 +0900

    func component example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Greeting({ food, beverage }) {
-  console.log();
+const koyotae = [
+  {
+    name: "sinji",
+    img:
+      "http://img.etoday.co.kr/pto_db/2015/09/20150904111731_706635_395_699.jpg",
+    rating: 1
+  },
+  {
+    name: "jongmin",
+    img:
+      "https://file.mk.co.kr/meet/neds/2014/01/image_readtop_2014_107902_13902783731185927.jpg",
+    rating: 2
+  },
+  {
+    name: "bbackga",
+    img: "http://image.munhwa.com/gen_news/201504/20150402MW14025396706_b.jpg",
+    rating: 3
+  }
+];
+
+function Greeting({ name, src, rating }) {
   return (
-    <h3>
-      this is {food} and {beverage}
-    </h3>
+    <div>
+      <h2>my name is {name}</h2>
+      <img src={src} alt={name} />
+      <h2>{rating}</h2>
+    </div>
   );
 }
 
+Greeting.propTypes = {
+  name: PropTypes.string.isRequired,
+  src: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
 function App() {
+  console.log(
+    <Greeting
+      name={koyotae[0].name}
+      src={koyotae[0].img}
+      rating={koyotae[0].rating}
+      key={1}
+    />
+  );
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
-      <Greeting food="bread" beverage="water" />.
-      <Greeting food="kimchi" beverage="saida" />
-      <Greeting food="meet" beverage="cola" />
+      {koyotae.map((member, index) => (
+        <Greeting
+          name={member.name}
+          src={member.img}
+          key={index}
+          rating={member.rating}
+        />
+      ))}
     </div>
   );
 }
